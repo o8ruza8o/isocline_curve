79c3449087ee79e4ee8eeffbad4ff535625754ed
Ruza Markov <ruza.markov@gmail.com>
2023-06-21 08:18:51 -0700

    mesh adjacency matrix

--- README.md
+++ README.md
@@ -5,17 +5,17 @@ Find a direction V with a shortest 0° isocline curve
 # Approach
 
 Isoline curve is constructed given a reference vector V and an angle A such that
-the angle between the normal to surface and V is (pi/2 - A) and we integrate its
-length.
+the angle between the surface normal anywhere on the curve and V is (pi/2 - A)
+then we integrate its length.
 
 In our case, the angle A = 0 and the 0° isocline is a curve with normals
 perpendicular to a given V. We are looking for the V with shortest isocline.
 
 Assuming a closed differentiable surface in mesh representation, get all the
 faces that have a normal close enough to perpendicular to the given vector and
-try to connect them in a non-trivial loop. The shortest of the boundaries of
-that loop approximates the isocline for that direction. Estimate its
-length. Then find V with shortest isocline length.
+try to connect them in a non-trivial loop. The shortest path of edges in that
+loop approximates the isocline for given direction. Estimate its length then
+find V with shortest isocline length.
 
 # Requirements
 
@@ -26,7 +26,7 @@ pip install openmesh
 
 # Run an example
 ```
-python find_isocline.py examples/bob_isotropic.obj 
+python find_isocline.py examples/bob_isotropic.obj
 ```
 
 ![Bob isocline](examples/bob00.png)
--- find_isocline.py
+++ find_isocline.py
@@ -5,13 +5,12 @@ import numpy as np
 import os
 
 tolerance = 1e-1
-
+large_num = 1e7
 
 # class for adjacency matrix representation of the graph
 class Graph():
-    def __init__(self, vertices):
-        self.V = len(vertices)
-        self.vertices = vertices
+    def __init__(self, n_verts):
+        self.V = n_verts
         self.graph = np.zeros((self.V, self.V))
  
     def printSolution(self, dist, path):
@@ -20,7 +19,7 @@ class Graph():
             print(node, "\t\t", dist[node])
  
     def minDistance(self, dist, sptSet):
-        min = 1e7
+        min = large_num
         for v in range(self.V):
             if dist[v] < min and sptSet[v] == False:
                 min = dist[v]
@@ -28,7 +27,7 @@ class Graph():
         return min_index
 
     def dijkstra(self, src):
-        dist = [1e7] * self.V
+        dist = [large_num] * self.V
         path = [np.inf] * self.V
         dist[src] = 0
         sptSet = [False] * self.V
@@ -40,43 +39,56 @@ class Graph():
                 if (self.graph[u][v] > 0 and sptSet[v] == False and dist[v] > dist[u] + self.graph[u][v]):
                     dist[v] = dist[u] + self.graph[u][v]
                     path[count] = u
-        self.printSolution(dist, path)
+        # self.printSolution(dist, path)
 
 def generate_directions(min_phi=0, max_phi=np.pi, min_theta=0, max_theta=2*np.pi):
     directions = []
     for phi in np.linspace(min_phi, max_phi, 10):
         for theta in np.linspace(min_theta, max_theta, 10):
             directions.append(np.array([np.sin(phi)*np.cos(theta), np.sin(phi)*np.sin(theta), np.cos(phi)]))
-    # return directions
-    return [directions[0]]
+    return directions
+    # return [directions[0]]
 
-def estimate_length(mesh, faces):
-    # Dijkstra to get the shortest path
+def adjacency_matrix(mesh):
+    n_verts = mesh.n_vertices()
+    adjacency = np.zeros((n_verts, n_verts))
+    for vh0 in mesh.vertices():
+        for vh1 in mesh.vv(vh0):
+            point0 = mesh.point(vh0)
+            point1 = mesh.point(vh1)
+            adjacency[vh0.idx()][vh1.idx()] = np.linalg.norm(point1 - point0)
+            adjacency[vh1.idx()][vh0.idx()] = adjacency[vh0.idx()][vh1.idx()]
+    return adjacency
+
+def vertices_from_faces(mesh, faces):
     vertices = set()
     for fh in faces:
         vertices.update([vh.idx() for vh in mesh.fv(fh)])
-    g = Graph(vertices)
-    vertice = list(vertices)
-    adjecency = np.zeros((len(vertices), len(vertices)))
+    return list(vertices)
+
+def select_from_adjacency_matrix(mesh, mesh_adjacency, faces):
+    vertices = vertices_from_faces(mesh, faces)
+    adjacency = np.zeros((len(vertices), len(vertices)))
     for i, vid0 in enumerate(vertices):
         for j, vid1 in enumerate(vertices):
-            for fh in faces:
-                face_vertices = [vh.idx() for vh in mesh.fv(fh)]
-                if vid1 in face_vertices and vid0 in face_vertices:
-                    point0 = mesh.points()[vid0]
-                    point1 = mesh.points()[vid1]
-                    adjecency[i][j] = np.linalg.norm(point1 - point0)
-                    adjecency[j][i] = np.linalg.norm(point1 - point0)
-    g.graph = adjecency
-    print(adjecency)
+            adjacency[i][j] = mesh_adjacency[vid0][vid1]
+    return adjacency
+
+def estimate_length(mesh, mesh_adjacency, faces):
+    # Dijkstra to get the shortest path
+    adjacency = select_from_adjacency_matrix(mesh, mesh_adjacency, faces)
+    g = Graph(adjacency.shape[0])
+    g.graph = adjacency
     g.dijkstra(0)
+    # Copout
     return len(faces)
 
-def main(file_name, visualize):
+def main(file_name, export):
     mesh = om.read_trimesh(file_name)
     mesh.request_face_normals()
     mesh.update_normals()
     mesh.request_face_colors()
+    mesh_adjacency = adjacency_matrix(mesh)
     face_normals = mesh.face_normals()
 
     isoline_lengths = []
@@ -86,24 +98,27 @@ def main(file_name, visualize):
         for i, face in enumerate(mesh.faces()):
             if np.abs(np.dot(face_normals[i], direction)) < tolerance:
                 perpendicular_faces.append(face)
-        isoline_lengths.append(estimate_length(mesh, perpendicular_faces))
+        isoline_lengths.append(estimate_length(mesh, mesh_adjacency, perpendicular_faces))
 
     min_direction = directions[isoline_lengths.index(max(isoline_lengths))]
-    print(min_direction, "direction chosen")
+    print(min_direction, "direction chosen with length", min(isoline_lengths))
     for i, face in enumerate(mesh.faces()):
-            if np.abs(np.dot(face_normals[i], min_direction)) < tolerance:
-                mesh.set_color(face, [0.6, 0.3, 0.3, 0])
-            else:
-                mesh.set_color(face, [0.3, 0.6, 0.3, 0])
-    name, ext = os.path.splitext(file_name)
-    om.write_mesh(name + '_labeled.obj', mesh, face_color=True)
+        if np.abs(np.dot(face_normals[i], min_direction)) < tolerance:
+            mesh.set_color(face, [0.6, 0.3, 0.3, 0])
+        else:
+            mesh.set_color(face, [0.3, 0.6, 0.3, 0])
+    if export:
+        name, ext = os.path.splitext(file_name)
+        export_filename = name + '_labeled.obj'
+        om.write_mesh(export_filename, mesh, face_color=True)
+        print("Saved labeled mesh to", export_filename) 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Find shortest isocline')
     parser.add_argument('file_name',
                         help='Input file name is required. Enter it!')
-    parser.add_argument('--visualize', required=False, action='store_true')
-    parser.set_defaults(visualize=False)
+    parser.add_argument('--export', required=False, action='store_true')
+    parser.set_defaults(export=False)
     args = parser.parse_args()
 
-    main(args.file_name, args.visualize)
+    main(args.file_name, args.export)
